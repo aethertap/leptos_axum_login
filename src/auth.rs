3b7b0cc26bc8a75a15464e9a1d7b812394cfde4a
erik Lee <erik@codecloak.org>
2024-08-22 11:50:13 -0500

    zed says it will compile...

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,9 @@
 use leptos::*;
 use sqlx::SqlitePool;
 use crate::user::User;
-use crate::sqlite_backend::SqliteBackend;
 
-#[server(Login,"auth","Url","login")]
+
+#[server(Login,"api","Url","login")]
 pub async fn login(username: String, password: String) -> Result<User,ServerFnError> {
     let conn:SqlitePool = use_context().expect("no database connection");
     todo!()
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+
+use cfg_if::cfg_if;
+use leptos_axum::handle_server_fns_with_context;
+
+cfg_if!{
+    if #[cfg(feature="ssr")] {
+        use axum_login::AuthSession;
+        use axum::{body::Body, extract::State};
+        use leptos_axum_auth::{app::App,sqlite_backend::SqliteBackend};
+        use leptos::*;
+        async fn server_fn_handler(auth_session: AuthSession<SqliteBackend>, request: http::Request<Body>){
+            handle_server_fns_with_context(move || {
+                provide_context(auth_session.clone());
+            }, request).await;
+        }
+
+        #[axum_macros::debug_handler]
+        async fn leptos_routes_handler(auth_session: AuthSession<SqliteBackend>, State(options): State<LeptosOptions>, req: http::Request<Body>) -> http::Response<Body>{
+                let handler = leptos_axum::render_app_to_stream_with_context(options.clone(),
+                move || {
+                    provide_context(auth_session.clone())
+                },
+                || view! { <App/> }
+            );
+            handler(req).await
+        }
+    }
+}
+
+
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use axum::Router;
+    use axum::routing::{get,post};
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use leptos_axum_auth::app::*;
+    use leptos_axum_auth::{app::*, sqlite_backend::SqliteBackend};
     use leptos_axum_auth::fileserv::file_and_error_handler;
+    use axum::ServiceExt;
+    use axum_login::{
+        login_required,
+        tower_sessions::{MemoryStore, SessionManagerLayer},
+        AuthManagerLayerBuilder,
+    };
 
     // Setting get_configuration(None) means we'll be using cargo-leptos's env values
     // For deployment these variables are:
@@ -17,11 +54,21 @@ async fn main() {
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
-    // build our application with a route
+    let session_store = MemoryStore::default();
+    let session_layer = SessionManagerLayer::new(session_store);
+    let backend = SqliteBackend::new()
+        .await.expect("failed to get auth backend");
+    backend.migrate()
+        .await.expect("failed database migration");
+    let auth_layer = AuthManagerLayerBuilder::new(backend, session_layer.clone()).build();
+
     let app = Router::new()
-        .leptos_routes(&leptos_options, routes, App)
+        .route("/api/*fn_name", post(server_fn_handler))
+        .leptos_routes_with_handler(routes, get(leptos_routes_handler) )
         .fallback(file_and_error_handler)
-        .with_state(leptos_options);
+        .layer(session_layer)
+        .layer(auth_layer)
+        .with_state(leptos_options) ;
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     logging::log!("listening on http://{}", &addr);
