71b76a05860092c816a4d7245afdb3285456daa2
Erik Lee <2141800+aethertap@users.noreply.github.com>
2024-08-25 07:29:34 -0500

    forgot to save a file before committing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #![allow(unused)]
 
+// Leptos beginners: Consider this file to be the true root of all of your code, even stuff that is only
+// for the server. When I started using leptos, I got mixed up and tried to split out server
+// code so that it was only `use`d from main.rs, and that was a giant mistake. Put everything here
+// and protect it with `cfg_if`. The only place where you'll need to qualify it with the crate
+// name is in `main.rs`.
+
 pub mod app;
 pub mod error_template;
 pub mod auth;
